c9181f3a34ff0a9050a7ae85a9ba1f6768557a61
cobrelli <cobrelli@gmail.com>
2012-12-15 10:50:49 +0200

    testejä

---
+++ Ohjelma/test/CharClassTests/CharPartyTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CharClassTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cobrelli
+ */
+public class CharPartyTest {
+    
+    public CharPartyTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+}
