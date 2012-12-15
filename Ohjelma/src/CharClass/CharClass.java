3fd6df45771ec9597f41eaf1743863e2d1e7f4f3
Cobrelli <Cobrelli@192.168.1.34>
2012-12-15 20:58:11 +0200

    Testejä ja javadocs

--- Ohjelma/src/CharClass/CharClass.java
+++ Ohjelma/src/CharClass/CharClass.java
@@ -1,9 +1,9 @@
 package CharClass;
 
 /**
- * Abstrakti yläluokka muille hahmoluokille. Sisältää keskeiset komennot aliluokkia,
- * kuten Fighter:a varten.
- * 
+ * Abstrakti yläluokka muille hahmoluokille. Sisältää keskeiset komennot
+ * aliluokkia, kuten Fighter:a varten.
+ *
  * @author Cobrelli
  */
 public abstract class CharClass {
@@ -17,80 +17,87 @@ public abstract class CharClass {
     private String charClassID;
     private int x;
     private int y;
-    
+
     /**
-     * Luokan konstruktori, jolla luodaan uusi hahmo halutulla nimellä 
+     * Luokan konstruktori, jolla luodaan uusi hahmo halutulla nimellä
      * varustettuna.
-     * 
-     * @param nimi      Hahmolle annettu nimi talletetaan oliolle.
+     *
+     * @param nimi Hahmolle annettu nimi talletetaan oliolle.
      */
     public CharClass(String nimi) {
 
         this.nimi = nimi;
         this.elossa = true;
         this.charClassID = "";
-        
+
     }
 
     /**
      * Asettaa hahmon paikan x -akselilla.
-     * @param x     Paikka x -akselilla kenttä matriisissa.
+     *
+     * @param x Paikka x -akselilla kenttä matriisissa.
      */
-    public void setX(int x){
+    public void setX(int x) {
         this.x = x;
     }
-    
+
     /**
      * Asettaa hahmon paikan y -akselilla.
-     * @param y     Paikka y -akselilla kenttä matriisissa.
+     *
+     * @param y Paikka y -akselilla kenttä matriisissa.
      */
-    public void setY(int y){
+    public void setY(int y) {
         this.y = y;
     }
-    
+
     /**
      * Palauttaa hahmon paikan x -akselilla.
-     * @return      Paikka x -akselillä kenttä matriisissa.
+     *
+     * @return Paikka x -akselillä kenttä matriisissa.
      */
-    public int getX(){
+    public int getX() {
         return this.x;
     }
-    
+
     /**
      * Palauttaa hahmon paikan y -akselilla.
-     * @return      Paikka x -akselillä kenttä matriisissa.
+     *
+     * @return Paikka x -akselillä kenttä matriisissa.
      */
-    public int getY(){
+    public int getY() {
         return this.y;
     }
-    
+
     /**
      * Alustaa täysiksi statseiksi max statsit.
      */
-    public void alustaStatsit(){
+    public void alustaStatsit() {
         this.Hp = HpMax;
         this.Mp = MpMax;
     }
-    
+
     /**
      * Hahmoluokan ID:n getteri
-     * @return      Palauttaa hahmoluokan CharClassID:n
+     *
+     * @return Palauttaa hahmoluokan CharClassID:n
      */
-    public String getCharClassID(){
+    public String getCharClassID() {
         return this.charClassID;
     }
-    
+
     /**
      * Hahmoluokan ID:n setteri
-     * @param ID    Asettaa CharClassID:lle uuden määritellyn Stringin.
+     *
+     * @param ID Asettaa CharClassID:lle uuden määritellyn Stringin.
      */
-    public void setCharClassID(String ID){
+    public void setCharClassID(String ID) {
         this.charClassID = ID;
     }
-    
+
     /**
      * Kasvattaa tämänhetkistä Hp määrää.
-     * @param maara     Asennettu parametri lisätään Hp attribuuttiin.
+     *
+     * @param maara Asennettu parametri lisätään Hp attribuuttiin.
      */
     public void lisaaHp(int maara) {
 
@@ -107,7 +114,8 @@ public abstract class CharClass {
 
     /**
      * Vahentaa nykyisen Hp:n määrää.
-     * @param maara     Hp:ta vähennetään maara:n verran
+     *
+     * @param maara Hp:ta vähennetään maara:n verran
      */
     public void vahennaHp(int maara) {
         if (!elossa) {
@@ -116,54 +124,60 @@ public abstract class CharClass {
         if (this.Hp - maara < 1) {
             this.elossa = false;
             this.Hp = -1;
-        }else{
+        } else {
             this.Hp -= maara;
         }
     }
 
     /**
      * Nykyisen Hp määrän getteri
-     * @return      Palauttaa nykyisen Hp määrän
+     *
+     * @return Palauttaa nykyisen Hp määrän
      */
     public int getHp() {
         return this.Hp;
     }
-    
+
     /**
      * HpMax Setteri
-     * @param uusi      Asettaa HpMaxin tähän arvoon.
+     *
+     * @param uusi Asettaa HpMaxin tähän arvoon.
      */
-    public void setHpMax(int uusi){
+    public void setHpMax(int uusi) {
         this.HpMax = uusi;
     }
-    
+
     /**
      * MpMax setteri
-     * @param uusi      Asettaa MpMaxin tähän arvoon.
+     *
+     * @param uusi Asettaa MpMaxin tähän arvoon.
      */
-    public void setMpMax(int uusi){
+    public void setMpMax(int uusi) {
         this.MpMax = uusi;
     }
-    
+
     /**
      * HpMax getteri
-     * @return      Palauttaa HpMax.
+     *
+     * @return Palauttaa HpMax.
      */
-    public int getHpMax(){
+    public int getHpMax() {
         return this.HpMax;
     }
-    
+
     /**
      * MpMax getteri
-     * @return      Palauttaa MpMax.
+     *
+     * @return Palauttaa MpMax.
      */
-    public int getMpMax(){
+    public int getMpMax() {
         return this.MpMax;
     }
-    
+
     /**
      * Lisää Mp attribuuttia maaran verran ja tarkistaa ettei ylitä maksimia
-     * @param maara     Lisataan nykyisiin Mp
+     *
+     * @param maara Lisataan nykyisiin Mp
      */
     public void lisaaMp(int maara) {
 
@@ -176,48 +190,51 @@ public abstract class CharClass {
 
     /**
      * Vähentää Mp attribuuttia maaran verran ja tarkistaa ettei ylitä maksimia
-     * @param maara     Vähennetään nykyisiin Mp
+     *
+     * @param maara Vähennetään nykyisiin Mp
      */
     public void vahennaMp(int maara) {
-        
+
         if (this.Mp - maara < 0) {
             this.Mp = 0;
-        }else{
+        } else {
             this.Mp -= maara;
         }
     }
 
     /**
      * Mp attribuutin getteri
-     * @return      Palauttaa Mp
+     *
+     * @return Palauttaa Mp
      */
     public int getMp() {
         return this.Mp;
     }
-    
+
     /**
      * getteri booleanille, joka kertoo onko kyseinen yksikkö elossa.
-     * @return      Palauttaa booleanin elossa
+     *
+     * @return Palauttaa booleanin elossa
      */
-    public boolean getIsAlive(){
+    public boolean getIsAlive() {
         return elossa;
     }
-    
+
     /**
+     * Käytetään elossaolon tulostukseen esim toStringissä.
      *
-     * @return
+     * @return Palauttaa tyhjää jos elossa, R.I.P, jos kuollut.
      */
-    public String onkoElossa(){
-        if(elossa){
+    public String onkoElossa() {
+        if (elossa) {
             return "";
         }
         return "R.I.P";
     }
-    
+
     @Override
-    public String toString(){
-        return this.nimi + " " + this.charClassID + " HP: (" + getHp() + ") MP: (" + getMp() + ") " 
+    public String toString() {
+        return this.nimi + " " + this.charClassID + " HP: (" + getHp() + ") MP: (" + getMp() + ") "
                 + onkoElossa();
     }
-    
 }
--- Ohjelma/test/CharClassTests/CharClassTest.java
+++ Ohjelma/test/CharClassTests/CharClassTest.java
@@ -1,37 +1,52 @@
 package CharClassTests;
 
 import CharClass.Fighter;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 /**
- * 
+ * Testaa CharClassLuokkaa
  * @author Cobrelli
  */
 public class CharClassTest {
     
     Fighter fighter;
     
+    /**
+     *
+     */
     public CharClassTest() {
     }
     
+    /**
+     * Pistää tarvittavat esitilanteet pystyyn.
+     */
     @Before
     public void setUp() {
         fighter = new Fighter("testi");
     }
     
+    /**
+     * Tarkistaa setterin toimivuuden
+     */
     @Test
     public void testaaEttaSetXAsettaaPaikanOikein(){
         fighter.setX(5);
         assertEquals(5, fighter.getX());
     }
     
+    /**
+     * Tarkistaa setterin toimivuuden
+     */
     @Test
     public void testaaEttaSetYAsettaaPaikanOikein(){
         fighter.setY(5);
         assertEquals(5, fighter.getY());
     }
     
+    /**
+     * Alustusmetodi muuttaa nykyiseksi hp:ksi max hp:n oikein.
+     */
     @Test
     public void testaaAlustusAsettaaHpOikein(){
         fighter.setHpMax(99);
@@ -39,6 +54,9 @@ public class CharClassTest {
         assertEquals(99, fighter.getHp());
     }
     
+    /**
+     * Alustusmetodi muuttaa nykyiseksi mp:ksi max mp:n oikein.
+     */
     @Test
     public void testaaAlustusAsettaaMpOikein(){
         fighter.setMpMax(99);
@@ -46,6 +64,9 @@ public class CharClassTest {
         assertEquals(99, fighter.getMp());
     }
     
+    /**
+     * Hahmolta vahennettava hp laskee oikein hp arvoa.
+     */
     @Test 
     public void testaaEttaHpVaheneeOikeasti(){
         int arvo = fighter.getHp();
@@ -54,6 +75,17 @@ public class CharClassTest {
         assertEquals(arvo-30, fighter.getHp());
     }
     
+    @Test
+    public void testaaEttaAlleNollanVahenevaHpEiPalauduPlussalle(){
+        fighter.vahennaHp((fighter.getHp()+20));
+        fighter.lisaaHp(30);
+        
+        assertEquals(-1, fighter.getHp());
+    }
+    
+    /**
+     * Hahmolta vahennettava mp laskee oikein mp arvoa.
+     */
     @Test 
     public void testaaEttaMpVaheneeOikeasti(){
         fighter.setMpMax(100);
@@ -66,6 +98,9 @@ public class CharClassTest {
         assertEquals(arvo-30, fighter.getMp());
     }
     
+    /**
+     * Hahmolle lisätty hp nostaa hp arvoa oikein
+     */
     @Test
     public void testaaEttaLisattyHpKasvattaaOikeastiMaaraa(){
         fighter.vahennaHp(50);
@@ -76,6 +111,9 @@ public class CharClassTest {
         assertEquals(arvo+10, fighter.getHp());
     }
     
+    /**
+     * Testaa lisaa mp toimivuuden
+     */
     @Test
     public void testaaEttaLisattyMpKasvattaaOikeastiMaaraa(){
         fighter.setMpMax(120);
@@ -86,16 +124,48 @@ public class CharClassTest {
         assertEquals(20, fighter.getMp());
     }
     
+    /**
+     * Hpmax setterin testaus
+     */
     @Test
     public void testaaEttaSetteriMuuttaaMaxHp(){
         fighter.setHpMax(10);
         assertEquals(10, fighter.getHpMax());
     }
     
+    /**
+     * Mpmax setterin testaus
+     */
     @Test
     public void testaaEttaSetteriMuuttaaMaxMp(){
         fighter.setMpMax(10);
         assertEquals(10, fighter.getMpMax());
     }
     
+    /**
+     * Elossa olevalle hahmolle palautetaan boolean elossa oikein.
+     */
+    @Test
+    public void getteriPalauttaaElossaOikein(){
+        assertTrue(fighter.getIsAlive());
+    }
+    
+    /**
+     * Jos hp laskee alle nollan niin hahmo muuttuu kuolleeksi niin kuin pitää.
+     */
+    @Test
+    public void HpnLaskuAlleNollaMuuttaaKuolleeksi(){
+        fighter.vahennaHp(100000);
+        assertFalse(fighter.getIsAlive());
+    }
+    
+    /**
+     * Jos hahmo kuolee ei lisää hp herätä sitä henkiin.
+     */
+    @Test
+    public void HpnLaskuAlleNollaMuuttaaKuolleeksiEikaHpNousuHerataHenkiin(){
+        fighter.vahennaHp(100000);
+        fighter.lisaaHp(100000);
+        assertFalse(fighter.getIsAlive());
+    }
 }
