c1abbd81fac046f32562bde656438253d24003cb
Cobrelli <Cobrelli@192.168.1.34>
2012-12-17 23:04:40 +0200

    piirtoalustalla piirtelyä

--- Ohjelma/src/GUI/PiirtoalustaTaistelu.java
+++ Ohjelma/src/GUI/PiirtoalustaTaistelu.java
@@ -1,21 +1,38 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Cobrelli
  */
-public class PiirtoalustaTaistelu extends JPanel implements Paivitettava{
+public class PiirtoalustaTaistelu extends JPanel implements Paivitettava {
 
-    public PiirtoalustaTaistelu(){
+    public PiirtoalustaTaistelu() {
         super.setBackground(Color.white);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        piirraRuudukko(g);
+    }
     
+        private void piirraRuudukko(Graphics g) {
+        g.setColor(Color.black);
+
+        for (int i = 100; i < 500; i += 25) {
+            g.fillRect(100, i, 376, 1);
+            g.fillRect(i, 100, 1, 376);
+        }
+
+    }
+
     @Override
     public void paivita() {
         repaint();
     }
-    
 }
