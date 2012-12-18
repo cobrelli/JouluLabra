5d85c27c605babce011fb79c96cd2f809cb38201
Cobrelli <Cobrelli@192.168.1.34>
2012-12-18 07:19:41 +0200

    Nappaimistonkuuntelijan suunnittelua

---
+++ Ohjelma/src/GUI/Nappaimistonkuuntelija/Nappaimistonkuuntelija.java
@@ -0,0 +1,29 @@
+package GUI.Nappaimistonkuuntelija;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Cobrelli
+ */
+public class Nappaimistonkuuntelija extends JPanel implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+}
