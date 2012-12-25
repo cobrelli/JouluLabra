f38208a71e666f4b4b9d664f193f0504d069ef25
Cobrelli <cobrelli@gmail.com>
2012-12-25 13:02:08 +0200

    kaupparuudun rakentelua

--- Ohjelma/src/GUI/GUI.java
+++ Ohjelma/src/GUI/GUI.java
@@ -16,8 +16,10 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
@@ -108,7 +110,7 @@ public class GUI implements Runnable {
 
         JarjestaOliot j = new JarjestaOliot(mash, party);
         j.jarjesta();
-        
+
         this.alusta = new PiirtoalustaTaistelu(mash, party, vuoro);
 
         container.add(this.alusta, BorderLayout.CENTER);
@@ -127,49 +129,55 @@ public class GUI implements Runnable {
         this.alusta = new PiirtoalustaKauppa();
         container.add(alusta);
 
-        alusta.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 60;
-        c.ipadx = 0;
-        c.gridx = 1;
-        c.gridy = 0;
+        alusta.setLayout(new BorderLayout(10, 10));
 
         Font font = new Font("Verdana", Font.BOLD, 30);
 
+        JPanel otsikko = new JPanel(new GridLayout(1, 3));
+        otsikko.setBackground(Color.white);
+        alusta.add(otsikko, BorderLayout.NORTH);
+        
         JTextArea Teksti1 = new JTextArea("Kauppa");
         Teksti1.setForeground(Color.black);
-        Teksti1.setBackground(Color.white);
         Teksti1.setEditable(false);
         Teksti1.setFont(font);
 
-        alusta.add(Teksti1, c);
+        otsikko.add(new JLabel());
+        otsikko.add(Teksti1);
+        otsikko.add(new JLabel());
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipadx = 0;
-        c.ipady = 20;
-        c.gridwidth = 1;
-        c.gridx = 1;
-        c.gridy = 1;
+        JPanel kaupanPainikkeet = new JPanel(new GridBagLayout());
+        kaupanPainikkeet.setBackground(Color.white);
+        alusta.add(kaupanPainikkeet, BorderLayout.CENTER);
 
-        JButton button1 = new JButton("Aloita taistelu");
-        alusta.add(button1, c);
+        JButton ostaNostovaki = new JButton("Nostoväki");
+        ostaNostovaki.setPreferredSize(new Dimension(130, 80));
+        JButton ostaSoturi = new JButton("Soturi");
+        ostaSoturi.setPreferredSize(new Dimension(130,80));
+        
+        kaupanPainikkeet.add(ostaNostovaki);
+        kaupanPainikkeet.add(ostaSoturi);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipadx = 0;
-        c.ipady = 20;
-        c.gridwidth = 1;
-        c.gridx = 1;
-        c.gridy = 2;
-
-        button1.addActionListener(new luoTaisteluruutu(frame, this));
-
-        JButton button2 = new JButton("Lopeta");
-        alusta.add(button2, c);
-        button2.addActionListener(new Exit());
-
-
+        JPanel hallintaNappulat = new JPanel(new GridBagLayout());
+        hallintaNappulat.setBackground(Color.white);
+        JButton aloita = new JButton("Taisteluun");
+        aloita.addActionListener(new luoTaisteluruutu(frame, this));
+        alusta.add(hallintaNappulat, BorderLayout.WEST);
+        hallintaNappulat.setPreferredSize(new Dimension(130, 80));
+        hallintaNappulat.add(aloita);
+        
+        JPanel pisteet = new JPanel(new GridLayout(1, 3));
+        pisteet.setBackground(Color.white);
+        JTextArea pisteLaskuri = new JTextArea("Pisteitä jäljellä: ");
+        pisteLaskuri.setForeground(Color.black);
+        pisteLaskuri.setEditable(false);
+        pisteLaskuri.setFont(font);
+        pisteLaskuri.setFont(new Font("Verdana", Font.BOLD, 12));
+        
+        alusta.add(pisteet, BorderLayout.SOUTH);
+        pisteet.add(new JLabel());
+        pisteet.add(pisteLaskuri, BorderLayout.SOUTH);
+        pisteet.add(new JLabel());
     }
 
     public JFrame getFrame() {
