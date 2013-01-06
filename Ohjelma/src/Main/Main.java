85060eef9974cdce4e8e33b4ed465301be6b2020
cobrelli <cobrelli@gmail.com>
2013-01-06 19:07:50 +0200

    refaktorointia

--- Ohjelma/src/Main/Main.java
+++ Ohjelma/src/Main/Main.java
@@ -1,20 +1,16 @@
-
 package Main;
 
-import GUI.GUI;
 import Peli.Peli;
-import Viholliset.HirvioRyhma;
-import Viholliset.Rotta;
-import Yksikot.Ryhma;
-import Yksikot.Soturi;
 
 /**
+ * Main luokka joka sisältää käynnistyksen yhteydessä ajettavan main metodin.
  *
  * @author Cobrelli
  */
 public class Main {
 
     /**
+     * Main metodi joka hoitaa pelin luomisen ja käynnistämisen
      *
      * @param args
      */
@@ -22,44 +18,5 @@ public class Main {
 
         Peli peli = new Peli();
         peli.kaynnista();
-        
-//        Soturi eka = new Soturi("eka");
-//        Soturi toka = new Soturi("toka");
-//        
-//        Ryhma p = new Ryhma();
-//        
-//        p.lisaaHahmo(eka);
-//        p.lisaaHahmo(toka);
-//        
-////        eka.setX(101);
-////        eka.setY(101);
-////        
-////        toka.setX(126);
-////        toka.setY(126);
-////        
-//        Rotta r = new Rotta("rat");
-//        
-//        HirvioRyhma m = new HirvioRyhma();
-//        m.lisaaMosa(r);
-//        
-////        r.setX(401);
-////        r.setY(401);
-//
-//        Rotta r2 = new Rotta("rotta");
-//        r2.setX(301);
-////        r2.setY(401);
-////        r2.setY(401);
-//        m.lisaaMosa(r2);
-//        
-//        Rotta r3 = new Rotta("rotta");
-////        r3.setX(301);
-////        r3.setY(301);
-//        m.lisaaMosa(r3);
-//        r.setHpMax(120);
-//        
-//        GUI gui = new GUI(m, p);
-//        gui.run();
-//        
-        
     }
 }
